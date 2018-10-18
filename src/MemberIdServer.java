12a1b42abfd15df16b34d353326f3e8b7ce94067
Jon Tseng <mnjunior1041@gmail.com>
2018-10-18 12:27:14 -0500

    Member ID class
    ID server class to save ID when serialization occurs.

--- src/MemberIdServer.java
+++ src/MemberIdServer.java
@@ -1,17 +1,70 @@
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.Serializable;
 
+/**
+ * Generates member ids.
+ * 
+ * @author Brahma Dathan and Sarnath Ramnath
+ *
+ */
 public class MemberIdServer implements Serializable {
+    private int idCounter;
+    private static MemberIdServer server;
 
-	public static MemberIdServer instance() {
-		// TODO Auto-generated method stub
-		return null;
+    /*
+     * Private constructor for singleton pattern
+     * 
+     */
+    private MemberIdServer() {
+        idCounter = 1;
+    }
 
-	}
+    /**
+     * Supports the singleton pattern
+     * 
+     * @return the singleton object
+     */
+    public static MemberIdServer instance() {
+        if (server == null) {
+            return (server = new MemberIdServer());
+        } else {
+            return server;
+        }
+    }
 
-	public static void retrieve(ObjectInputStream input) {
-		// TODO Auto-generated method stub
+    /**
+     * Getter for id
+     * 
+     * @return id of the member
+     */
+    public int getId() {
+        return idCounter++;
+    }
 
-	}
+    /**
+     * String form of the collection
+     * 
+     */
+    @Override
+    public String toString() {
+        return ("IdServer" + idCounter);
+    }
+
+    /**
+     * Retrieves the server object
+     * 
+     * @param input
+     *            inputstream for deserialization
+     */
+    public static void retrieve(ObjectInputStream input) {
+        try {
+            server = (MemberIdServer) input.readObject();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } catch (Exception cnfe) {
+            cnfe.printStackTrace();
+        }
+    }
 
 }
