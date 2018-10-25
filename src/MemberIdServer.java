f25071d327237027142bf6758559414022d5aa73
Stephen Thomas <smthomas@csbsju.edu>
2018-10-24 23:18:43 -0500

    MemberIdServer change

--- src/MemberIdServer.java
+++ src/MemberIdServer.java
@@ -9,62 +9,61 @@ import java.io.Serializable;
  *
  */
 public class MemberIdServer implements Serializable {
-    private int idCounter;
-    private static MemberIdServer server;
+	private int idCounter;
+	private static MemberIdServer server;
 
-    /*
-     * Private constructor for singleton pattern
-     * 
-     */
-    private MemberIdServer() {
-        idCounter = 1;
-    }
+	/**
+	 * Private constructor for singleton pattern
+	 * 
+	 */
+	private MemberIdServer() {
+		idCounter = 1;
+	}
 
-    /**
-     * Supports the singleton pattern
-     * 
-     * @return the singleton object
-     */
-    public static MemberIdServer instance() {
-        if (server == null) {
-            return (server = new MemberIdServer());
-        } else {
-            return server;
-        }
-    }
+	/**
+	 * Supports the singleton pattern
+	 * 
+	 * @return the singleton object
+	 */
+	public static MemberIdServer instance() {
+		if (server == null) {
+			return (server = new MemberIdServer());
+		} else {
+			return server;
+		}
+	}
 
-    /**
-     * Getter for id
-     * 
-     * @return id of the member
-     */
-    public int getId() {
-        return idCounter++;
-    }
+	/**
+	 * Getter for id
+	 * 
+	 * @return id of the member
+	 */
+	public int getId() {
+		return idCounter++;
+	}
 
-    /**
-     * String form of the collection
-     * 
-     */
-    @Override
-    public String toString() {
-        return ("IdServer" + idCounter);
-    }
+	/**
+	 * String form of the collection
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return ("IdServer" + idCounter);
+	}
 
-    /**
-     * Retrieves the server object
-     * 
-     * @param input
-     *            inputstream for deserialization
-     */
-    public static void retrieve(ObjectInputStream input) {
-        try {
-            server = (MemberIdServer) input.readObject();
-        } catch (IOException ioe) {
-            ioe.printStackTrace();
-        } catch (Exception cnfe) {
-            cnfe.printStackTrace();
-        }
-    }
+	/**
+	 * Retrieves the server object
+	 * 
+	 * @param input inputstream for deserialization
+	 */
+	public static void retrieve(ObjectInputStream input) {
+		try {
+			server = (MemberIdServer) input.readObject();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} catch (Exception cnfe) {
+			cnfe.printStackTrace();
+		}
+	}
 
 }
