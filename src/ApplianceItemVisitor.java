41970d871403d21d2f32cc1d6e90c1f6f6f96991
Jon Tseng <mnjunior1041@gmail.com>
2018-11-10 11:48:47 -0600

    Edited documentation

--- src/ApplianceItemVisitor.java
+++ src/ApplianceItemVisitor.java
@@ -1,47 +1,47 @@
 /**
  * This is the standard Visitor interface for the visitor pattern.
  * 
- * @author Brahma Dathan
+ * @author Jonathan Tseng
  *
  */
 public interface ApplianceItemVisitor {
 	/**
-	 * This is the method for catching all LoanableItem objects that may be
+	 * This is the method for catching all ApplianceItems objects that may be
 	 * added in the future.
 	 * 
 	 * @param item
-	 *            the item to be processed
+	 *            the ApplianceItem to be processed
 	 */
 	public void visit(ApplianceItem item);
 
 	/**
-	 * This method processes Book objects
+	 * This method processes Refrigerator objects
 	 * 
-	 * @param book
-	 *            the Book to be processed
+	 * @param refrigerator
+	 *            the refrigerator to be processed
 	 */
 	public void visit(Refrigerator refrigerator);
 
 	/**
-	 * The method processes Periodical objects
+	 * The method processes ClothesDryer objects
 	 * 
-	 * @param periodical
-	 *            the Periodical to be processed
+	 * @param clothesDryer
+	 *            the ClothesDryer to be processed
 	 */
 	public void visit(ClothesDryer clothesDryer);
 	/**
-	 * The method processes Periodical objects
+	 * The method processes ClothesWasher objects
 	 * 
-	 * @param periodical
-	 *            the Periodical to be processed
+	 * @param clothesWasher
+	 *            the clothes washer to be processed
 	 */
 	public void visit(ClothesWasher clothesWasher);
 
 	/**
- 	* The method processes Periodical objects
+ 	* The method processes Furnace objects
  	* 
- 	* @param periodical
- 	*            the Periodical to be processed
+ 	* @param furnance
+ 	*            the furnace to be processed
  	*/
 	public void visit(Furnace furnance);
 	
